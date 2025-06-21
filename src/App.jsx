68edd3c1b92632f4f81f7e245934334b393b290f
Geoespi <abrahamjime@hotmail.com>
2025-06-21 11:40:51 +0200

    added programs section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './components/navbar/NavbarComponent'
 import Header from './components/header/HeaderComponent'
+import Programs from './components/programs/ProgramsComponent'
+import Title from './components/title/TitleComponent'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className="container">
+        <Title subtitle="Our Programs" title="What We Offer"/>
+        <Programs />
+      </div>
     </div>
   )
 }
